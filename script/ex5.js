9a22b577358abc48a6563d385584dc55be7b389f
CauaGNP <cauagnp13@gmail.com>
2024-09-09 16:19:33 -0300

    ex5- tela login feita(falta mais uma tela)

--- script/ex5.js
+++ script/ex5.js
@@ -1,3 +1,28 @@
 let check = document.querySelector('.checkbutton');
-let button = document.querySelector('#submitButton')
+let button = document.querySelector('#submitButton');
+let mainLogin = document.querySelector('#mainloginId');
 
+button.addEventListener('click', () => {
+      let user = document.querySelector('#userId');
+      let password = document.querySelector('#senhaId');
+      if (user.value != 'cleber123' || password.value != '1234'){
+        alert('Nome do usuário ou senha incorreta')
+        user.value = '';
+        password.value = '';
+        user.focus();
+      }else{
+        mainLogin.style.display = 'none' 
+      }
+})
+
+check.addEventListener('click', () =>{
+    let password = document.querySelector('#senhaId');
+    let verificar = password.getAttribute('type');
+
+    if (verificar == 'password'){
+        password.type = "text";
+    }else{
+        password.type = "password"
+    }
+
+})
