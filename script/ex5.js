e41858c02a379ace972619ecae33dae55aed4374
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2024-09-10 22:05:34 +0000

    ajustes

--- script/ex5.js
+++ script/ex5.js
@@ -29,3 +29,12 @@ check.addEventListener('click', () =>{
 
 })
 
+let image = document.getElementById('selectImageId')
+let img = document.querySelector('#imagem')
+
+image.addEventListener('change', ()=>{
+    imagemSRC = 
+    image.value;
+    console.log(imagemSRC)
+    img.src = imagemSRC
+})
