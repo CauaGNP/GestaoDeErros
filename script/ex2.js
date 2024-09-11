c81d91b487a8cbcb8043494104d9401f32c415bf
CauaGNP <cauagnp13@gmail.com>
2024-09-11 11:12:45 -0300

    Finalizando exercicio 5

--- script/ex2.js
+++ script/ex2.js
@@ -1,13 +1,9 @@
-const changeImage = () => {
+let image = document.getElementById('selectImageId')
+let img = document.querySelector('.imagem')
 
-    // Pega o input
-    let image = document.getElementById('selectImageId');
-
-    // O 'image.files[0]' serve para pegar o primeiro arquivo do input file, pois pode haver vários arquivos
-    // O 'URL.createObjectUrl' serve para converter esse arquivo em um 'blob' (binary large object). O atributo src da tag img também aceita blobs como imagens
-    let newImageSrc = URL.createObjectURL(image.files[0]);
-
-    // Por fim, pego a minha tag img e altero o src dela por meio do setAttribute, onde o primeiro parâmetro é o atributo que quero alterar, nesse caso, o 'src', e o segundo
-    // o valor que quero colocar
-    document.getElementById('image').setAttribute('src', newImageSrc)
-}
+image.addEventListener('change', ()=>{
+    imagemSRC = 
+    image.value;
+    console.log(imagemSRC)
+    img.setAttribute('src', imagemSRC)
+})
